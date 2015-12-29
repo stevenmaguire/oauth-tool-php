a4987f10c3c18045eb50821389b334a08a24d385
Steven Maguire <stevenmaguire@gmail.com>
2015-12-29 16:09:51 -0600

    Update external references to be protocol sensitive

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>OAuth Tool | Helps you create access tokens for your development environment</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.min.css" rel="stylesheet" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.min.css" rel="stylesheet" />
     <style>section { padding: 3% 0; }</style>
     @yield('styles')
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/js/foundation.min.js"></script>
